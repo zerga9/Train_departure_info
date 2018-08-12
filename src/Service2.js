b58bbf210c6c6e94be444a9a25d475f90570c12d
Gadiza Zerari <gadizaz@hotmail.com>
2018-08-12 23:21:22 +0100

    Styling and info displayed for service 1 and 2

--- src/Service2.js
+++ src/Service2.js
@@ -1,8 +1,83 @@
 import React, { Component } from "react";
+import data from "./data/WAT-service-origin-W92443-2018-04-24.json";
+import moment from "moment";
+import {
+  Container,
+  styles,
+  TitleContainer,
+  Col1,
+  Col2,
+  Container2,
+  Col3,
+  SecondSp
+} from "./serviceDetailsStyles";
 
-class Service2 extends Component {
+class Service1 extends Component {
   render() {
-    return <div>Hello World form service2!</div>;
+    const allStops = data.service.stops;
+
+    return (
+      <div>
+        <Container>
+          <TitleContainer style={styles}>
+            <div>
+              <div>
+                <span>Waterloo to Shepperton</span>
+                <ul>operated by {data.service.serviceOperator}</ul>
+              </div>
+              <div>
+                {allStops.map(function(stop) {
+                  return (
+                    <Container2 key={stop.location.crs}>
+                      <Col1>
+                        {stop.arrival.scheduled
+                          ? moment(stop.arrival.scheduled.scheduledTime).format(
+                              "HH:mm"
+                            )
+                          : moment(
+                              stop.departure.scheduled.scheduledTime
+                            ).format("HH:mm")}
+                      </Col1>
+                      <Col2>
+                        <SecondSp>{stop.location.crs}</SecondSp>
+                      </Col2>
+                      <Col3>
+                        Plat.
+                        {stop.departure.scheduled
+                          ? stop.departure.scheduled.scheduledPlatform
+                          : "--"}
+                        <SecondSp>
+                          {stop.departure.scheduled &&
+                          !stop.departure.realTime.realTimeServiceInfo
+                            .hasDeparted
+                            ? "Est " +
+                              moment(
+                                stop.departure.realTime.realTimeServiceInfo
+                                  .realTime
+                              ).format("HH:mm")
+                            : stop.departure.scheduled &&
+                              stop.departure.realTime.realTimeServiceInfo
+                                .hasDeparted
+                              ? "Dep " +
+                                moment(
+                                  stop.departure.realTime.realTimeServiceInfo
+                                    .realTime
+                                ).format("HH:mm")
+                              : "Est " +
+                                moment(
+                                  stop.arrival.scheduled.scheduledTime
+                                ).format("HH:mm")}
+                        </SecondSp>
+                      </Col3>
+                    </Container2>
+                  );
+                })}
+              </div>
+            </div>
+          </TitleContainer>
+        </Container>
+      </div>
+    );
   }
 }
-export default Service2;
+export default Service1;
---
+++ src/serviceDetailsStyles.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #8c9190;
+  min-height: 600px;
+  overflow-y: auto;
+  width: 50%;
+  margin-left: 7.5px;
+  padding: 5px 15px 0;
+  box-sizing: border-box;
+  border-radius: 5px;
+`;
+
+export const Container2 = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  border-bottom: 1px solid #eee;
+  border-left: "3px solid white";
+  padding: 15px;
+  background-color: white;
+`;
+export const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center",
+  backgroundColor: "#d1f4ec",
+  width: "95%",
+  padding: "5px 15px 0"
+};
+
+export const TitleContainer = styled.div`
+  display: flex;
+  margin-bottom: 15px;
+  position: relative;
+`;
+
+export const Col1 = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-content: center;
+  justify-content: center;
+  span {
+    font-size: 18px;
+    font-weight: bold;
+  }
+`;
+
+export const Col2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 2;
+`;
+
+export const Col3 = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 2;
+`;
+
+export const SecondSp = styled.span`
+  font-size: 14px;
+  color: #8c9190;
+  align: right;
+`;
