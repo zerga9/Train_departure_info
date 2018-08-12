7b59e246a7071933245df1967ce7417cbafccb1a
Gadiza Zerari <gadizaz@hotmail.com>
2018-08-12 22:19:42 +0100

    Add background style color

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 import data from "./data/wat-departures.json";
 import moment from "moment";
 
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center",
+  backgroundColor: "#d1f4ec",
+  width: "100%",
+  padding: "5px 15px 0"
+};
+
 class App extends Component {
   render() {
     const allServices1 = data.services;
@@ -9,7 +17,7 @@ class App extends Component {
       service => service.destinationList[0]["crs"] !== "WAT"
     );
     return (
-      <div>
+      <div style={styles}>
         {allServices.map(service => {
           return (
             <div
