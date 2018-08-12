4552a958c5a5ed70ce9121165cca8497869b4250
Gadiza Zerari <gadizaz@hotmail.com>
2018-08-12 23:12:23 +0100

    HahRouter working with one file

---
+++ src/Service1.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Service1 extends Component {
+  render() {
+    return <div>Hello World!</div>;
+  }
+}
+export default Service1;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Service1 from "./Service1";
+import { HashRouter, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <HashRouter>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/serviceDetails/W92931" component={Service1} />
+    </div>
+  </HashRouter>,
+  document.getElementById("root")
 );
