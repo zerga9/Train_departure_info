536226ea33180f6f566ca5ba4aa7abc43bc286f2
Gadiza Zerari <gadizaz@hotmail.com>
2018-08-12 23:17:15 +0100

    Second service page routing

---
+++ src/Service2.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Service2 extends Component {
+  render() {
+    return <div>Hello World form service2!</div>;
+  }
+}
+export default Service2;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Service1 from "./Service1";
+import Service2 from "./Service2";
 import { HashRouter, Route } from "react-router-dom";
 
 ReactDOM.render(
@@ -9,6 +10,7 @@ ReactDOM.render(
     <div>
       <Route exact path="/" component={App} />
       <Route path="/serviceDetails/W92931" component={Service1} />
+      <Route path="/serviceDetails/W92443" component={Service2} />
     </div>
   </HashRouter>,
   document.getElementById("root")
