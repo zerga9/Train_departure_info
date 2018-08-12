b2a09d8602554a89c9e674e9c563e15a4320a545
Gadiza Zerari <gadizaz@hotmail.com>
2018-08-12 22:23:33 +0100

    Refactor styles in trainDeparturesStyles.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
 import data from "./data/wat-departures.json";
 import moment from "moment";
-
-const styles = {
-  fontFamily: "sans-serif",
-  textAlign: "center",
-  backgroundColor: "#d1f4ec",
-  width: "100%",
-  padding: "5px 15px 0"
-};
+import { styles } from "./trainDeparturesStyles";
 
 class App extends Component {
   render() {
---
+++ src/trainDeparturesStyles.js
@@ -0,0 +1,7 @@
+export const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center",
+  backgroundColor: "#d1f4ec",
+  width: "100%",
+  padding: "5px 15px 0"
+};
