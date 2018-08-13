70992af66becf19c7c83db55270524173d6763d0
Gadiza Zerari <gadizaz@hotmail.com>
2018-08-13 02:23:13 +0100

    Test for content first service passing

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -16,3 +16,10 @@ describe("Homepage got right content", function() {
     cy.contains("Departures from London Waterloo");
   });
 });
+
+describe("Homepage got right content", function() {
+  it('finds the content "Departures from London Waterloo "', function() {
+    cy.visit("http://localhost:3000/#/serviceDetails/W92931");
+    cy.contains("Waterloo to BasingStoke");
+  });
+});
